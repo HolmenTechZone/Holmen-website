3d63da5f4908a86a20d62cd4c287b8a1817c345c
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-12-11 13:26:22 +0100

    fixes: Navbar minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+
+      // Define custom colors
+
+      colors: {
+        Gray: "#A9A2A2",
+        Pale_lavender_pink: "#FACDFF",
+        Medium_orchid_purple: "#B262BA",
+      },
+    },
   },
   plugins: [],
-}
+};
