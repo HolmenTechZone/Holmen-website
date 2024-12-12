0b2216d8fd6f2d1e9c547d4b583d8eb1c9766920
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-12-12 11:47:13 +0100

    feat: Added the Navbar components to the other pages

--- src/Pages/About/About.vue
+++ src/Pages/About/About.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>Hello world</h1>
+    <div>
+        <Navbar/>
+        
+    </div>
+    
 </template>
+
+
+
+<script>
+import Navbar from '../../components/Navbar/Navbar.vue';
+
+
+
+
+
+export default {
+    components: {
+        Navbar
+
+    }
+}
+</script>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,14 +1,8 @@
 <template>
-  <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b">
-    <div
-      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
-    >
+  <nav class="bg-white fixed w-full z-20 top-0 start-0">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <div>
-        <img
-          src="../../assets/Logo/Holmen logo.svg"
-          alt="Holmen Logo"
-          class="h-8 md:h-10"
-        />
+        <img src="../../assets/Logo/Holmen logo.svg" alt="Holmen Logo" class="h-8 md:h-10" />
       </div>
 
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -20,61 +14,34 @@
         </button>
 
         <button
-          data-collapse-toggle="navbar-sticky"
+          @click="toggleMenu"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-sticky"
           aria-expanded="false"
         >
           <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
       </div>
 
-      <div
-        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-        id="navbar-sticky"
-      >
-        <ul
-          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-9 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white"
-        >
-          <li
-            class="block py-2 px-3 text-white bg-Gray rounded md:bg-transparent md:p-0 md:dark:text-Gray font-bold dark:hover:text-[#4E4545]"
-          >
+      <div :class="{'hidden': !isMenuOpen, 'block': isMenuOpen}" class="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-[#E6B3E6] rounded-lg  md:space-x-9 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+          <li class="block py-2 px-3 text-gray-700  rounded md:bg-transparent md:p-0 md:dark:text-gray-400 font-bold dark:hover:text-[#4E4545]">
             <router-link to="/" class="nav-link">Home</router-link>
           </li>
-          <li
-            class="block py-2 px-3 text-white bg-Gray rounded md:bg-transparent md:p-0 md:dark:text-Gray font-bold dark:hover:text-[#4E4545]"
-          >
+          <li class="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:p-0 md:dark:text-gray-400 font-bold dark:hover:text-[#4E4545]">
             <router-link to="/about">About</router-link>
           </li>
-          <li
-            class="block py-2 px-3 text-white bg-Gray rounded md:bg-transparent md:p-0 md:dark:text-Gray font-bold dark:hover:text-[#4E4545]"
-          >
+          <li class="block py-2 px-3 text-gray-700 rounded md:bg-transparent md:p-0 md:dark:text-gray-400 font-bold dark:hover:text-[#4E4545]">
             <router-link to="/services">Solutions & Services</router-link>
           </li>
-          <li
-            class="block py-2 px-3 text-white bg-Gray rounded md:bg-transparent md:p-0 md:dark:text-Gray font-bold dark:hover:text-[#4E4545]"
-          >
+          <li class="block py-2 px-3 text-gray-700  rounded md:bg-transparent md:p-0 md:dark:text-gray-400 font-bold dark:hover:text-[#4E4545]">
             <router-link to="/projects">Projects</router-link>
           </li>
-          <li
-            class="block py-2 px-3 text-white bg-Gray rounded md:bg-transparent md:p-0 md:dark:text-Gray font-bold dark:hover:text-[#4E4545]"
-          >
+          <li class="block py-2 px-3 text-gray-700  rounded md:bg-transparent md:p-0 md:dark:text-gray-400 font-bold dark:hover:text-[#4E4545]">
             <router-link to="/contact">Contact</router-link>
           </li>
         </ul>
@@ -82,3 +49,18 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
